d8e3f984907314969bb05c88b1a8671d31695142
Felipe <machadofelipe2016@outlook.com>
2022-07-20 14:29:45 -0300

    c9-adding context provider to manage code

--- blog-nutri/src/App.js
+++ blog-nutri/src/App.js
@@ -2,23 +2,32 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Footer from './Component/Footer';
 import Navbar from './Component/Navbar';
+import { AuthProviderContext } from './Contextmanage/authcontext';
 import Home from './pages/Home/Home';
+import Sobre from './pages/Sobre/sobre';
+import RegisterUser from './Register/registerUser';
 
 
 function App() {
   return (
     <div className="App">
+    <AuthProviderContext value={{}} >
       <BrowserRouter>
       <Navbar/>
         <Routes>
           <Route  path='/' element={<Home/>} />
-          <Route/>
-          <Route/>
+          
+           
+          <Route path='/login'  />
+          <Route path='/register' element={ <RegisterUser/> } />
+          <Route path='/about' element={<Sobre/>} />
           <Route/>
           <Route/>
         </Routes>
         <Footer/>
      </BrowserRouter>
+
+    </AuthProviderContext> 
     </div>
   );
 }
