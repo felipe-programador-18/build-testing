35019936c26985a7eb13283002bfa92db0d5846a
Felipe <machadofelipe2016@outlook.com>
2022-07-20 14:24:46 -0300

    c1-create folder register to user make register

---
+++ blog-nutri/src/Register/registerUser.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from "./register.module.css"
+
+
+const RegisterUser = () => {
+
+
+
+return(<div>
+          <h1>Register your date!!!</h1>
+
+</div>)
+}
+
+export default RegisterUser
