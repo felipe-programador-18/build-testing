cc27e10467c376562428c9d95c22299e656b3bb8
Felipe <machadofelipe2016@outlook.com>
2022-07-20 14:27:47 -0300

    c5-create component about

---
+++ blog-nutri/src/pages/Sobre/sobre.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from "./sobre.module.css"
+
+const Sobre = () => {
+    return(<div className={styles.about} >
+
+        <h3>Projeto realizado com react a parte front-end e a parte back-end com Firebase-Firestore </h3>
+         
+        <p>Linkdlin:</p>
+        <p>github</p>
+    
+    </div>)
+}
+
+export default Sobre
