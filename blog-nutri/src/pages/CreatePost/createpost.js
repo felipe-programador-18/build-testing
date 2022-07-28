a6d7fa016baa61c47d3c8cfff3ceff2aa2c58e2a
Felipe <machadofelipe2016@outlook.com>
2022-07-28 15:40:34 -0300

    c24-started create all structure about createpost

---
+++ blog-nutri/src/pages/CreatePost/createpost.js
@@ -0,0 +1,16 @@
+import React from 'react' 
+
+
+
+const CreatePost = () => {
+
+
+
+    return(<div className='d-flex flex-collumn' >
+           <h1>Create post here</h1>
+
+    </div>)
+
+}
+
+export default CreatePost
