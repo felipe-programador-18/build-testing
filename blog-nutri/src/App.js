6c0e68e74121d654bca3807b0ace950bdc899ad9
Felipe <machadofelipe2016@outlook.com>
2022-07-28 12:16:36 -0300

    c25-adding useState and effect to make autentication

--- blog-nutri/src/App.js
+++ blog-nutri/src/App.js
@@ -1,19 +1,35 @@
+import { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+
 import Footer from './Component/Footer';
 import Navbar from './Component/Navbar';
 import { AuthProviderContext } from './Contextmanage/authcontext';
+import { useAuth } from './hoock/useAuthTentication';
+import Dashboard from './pages/dashbords/dashboards';
 import Home from './pages/Home/Home';
 import Sobre from './pages/Sobre/sobre';
 import RegisterUser from './Register/registerUser';
 
 
 function App() {
+  const[user, setUsers] = useState(undefined)
+  const{auth} = useAuth()
+
+  console.log('test users here',user)
+  
+  useEffect(() => {
+
+  }, [])
+
+      
+  
   return (
     <div className="App">
-    <AuthProviderContext value={{}} >
+    <AuthProviderContext value={{user}} >
       <BrowserRouter>
       <Navbar/>
+      <div className='container' >
         <Routes>
           <Route  path='/' element={<Home/>} />
           
@@ -21,10 +37,13 @@ function App() {
           <Route path='/login'  />
           <Route path='/register' element={ <RegisterUser/> } />
           <Route path='/about' element={<Sobre/>} />
-          <Route/>
+          <Route path="/dashboard" element={<Dashboard/>}  />
           <Route/>
         </Routes>
+
+        </div>
         <Footer/>
+    
      </BrowserRouter>
 
     </AuthProviderContext> 
