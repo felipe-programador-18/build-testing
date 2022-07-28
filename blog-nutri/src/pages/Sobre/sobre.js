f2ea6e90a6bbabfb63ea9c1de71b24d8f2c63a4e
Felipe <machadofelipe2016@outlook.com>
2022-07-28 12:08:55 -0300

    c15-trade phrase about code

--- blog-nutri/src/pages/Sobre/sobre.js
+++ blog-nutri/src/pages/Sobre/sobre.js
@@ -4,7 +4,7 @@ import styles from "./sobre.module.css"
 const Sobre = () => {
     return(<div className={styles.about} >
 
-        <h3>Projeto realizado com react a parte front-end e a parte back-end com Firebase-Firestore </h3>
+        <h3>Projeto realizado com react a parte front-end e a parte back-end com Firebase-Firestore</h3>
          
         <p>Linkdlin:</p>
         <p>github</p>
