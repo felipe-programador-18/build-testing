b1706a8d55a2b201cf5776e2b13232d382144011
Felipe <machadofelipe2016@outlook.com>
2022-07-28 12:05:50 -0300

    c12-create part of dashboards of users

---
+++ blog-nutri/src/pages/dashbords/dashboards.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './dash.module.css'
+
+const Dashboard = () => {
+  return(<div>
+    
+    <h1>Dashboard here!</h1>
+
+  </div>)
+
+
+}
+
+export default Dashboard
