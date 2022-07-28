f64fdd896de66342b59dfee941f6cd87779f87a3
Felipe <machadofelipe2016@outlook.com>
2022-07-28 13:35:40 -0300

    c23-create and adding new routes

--- blog-nutri/src/App.js
+++ blog-nutri/src/App.js
@@ -1,11 +1,13 @@
+import { onAuthStateChanged } from 'firebase/auth';
 import { useEffect, useState } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 
 import Footer from './Component/Footer';
 import Navbar from './Component/Navbar';
 import { AuthProviderContext } from './Contextmanage/authcontext';
 import { useAuth } from './hoock/useAuthTentication';
+import CreateLogin from './Login/login';
 import Dashboard from './pages/dashbords/dashboards';
 import Home from './pages/Home/Home';
 import Sobre from './pages/Sobre/sobre';
@@ -15,14 +17,20 @@ import RegisterUser from './Register/registerUser';
 function App() {
   const[user, setUsers] = useState(undefined)
   const{auth} = useAuth()
-
+   
+  const loadeduser = user === undefined
   console.log('test users here',user)
   
   useEffect(() => {
+    onAuthStateChanged(auth,(user) => {
+    setUsers(user)
+    })
+  }, [auth])
 
-  }, [])
-
-      
+  if(loadeduser){
+    <p>Loading.....</p>
+  } 
+  
   
   return (
     <div className="App">
@@ -32,13 +40,16 @@ function App() {
       <div className='container' >
         <Routes>
           <Route  path='/' element={<Home/>} />
-          
-           
-          <Route path='/login'  />
-          <Route path='/register' element={ <RegisterUser/> } />
           <Route path='/about' element={<Sobre/>} />
           <Route path="/dashboard" element={<Dashboard/>}  />
+          
+          <Route path='' element={''}  />
+           
+          <Route path='/login' element={<CreateLogin/>}  />
+          <Route path='/register' element={!user ?  <RegisterUser/> : <Navigate to='/' /> } />
           <Route/>
+        
+        
         </Routes>
 
         </div>
