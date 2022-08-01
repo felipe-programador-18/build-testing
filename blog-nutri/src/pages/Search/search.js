bc13a4a93155196a3092fe69b662aea82fc06892
Felipe <machadofelipe2016@outlook.com>
2022-08-01 16:47:36 -0300

    c40-starting create part of search users

---
+++ blog-nutri/src/pages/Search/search.js
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom'
+
+import React from 'react'
+import { useFethingDocuments } from '../../hoock/useFecthingdates'
+
+
+const SearchDates = () => {
+
+  
+
+
+    return(<div className='d-flex justify-content-center align-items-center' >
+           
+           <h1>Testing searching here!!</h1>
+
+    </div>)
+}
+
+export default SearchDates
