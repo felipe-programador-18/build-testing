7d52b1b76636ae43bfc747d1f672ccb570425d3d
Felipe <machadofelipe2016@outlook.com>
2022-08-01 18:34:01 -0300

    c57- styles page of detauls

--- blog-nutri/src/Component/Post.details.js
+++ blog-nutri/src/Component/Post.details.js
@@ -2,13 +2,14 @@ import { Link } from "react-router-dom"
 
 const DetalsPost = ({posts}) => {
     console.log("testing here", posts)
-    return(<div className="d-flex justify-content-center align-items-center" >
-        <div>
-            <p>{posts.title} - {posts.createdBy} </p>
-        </div>
+    return(<div className="align-items-center " >
+            <img src={posts.image}  alt={posts.title} className='img-fluid' />
+            <h2 className="mx-2" >{posts.title}</h2>
+            <p className="mx-2" >{posts.createdBy}</p>
 
-        {posts && posts.tags.map((tag) => (<p key={tag} >
-            <span> {tag.tag} </span> 
+
+        {posts && posts.tags.map((tag) => (<p className="flex mx-2 " key={tag} >
+            <span> {tag} </span> 
         </p>))}
         
          
