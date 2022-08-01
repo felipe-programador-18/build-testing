86a98ab6a3b78ec14b95a05f0d57a83ff8e860dd
Felipe <machadofelipe2016@outlook.com>
2022-08-01 17:58:22 -0300

    c52-adding component to adding inside fstucture home

--- blog-nutri/src/pages/Home/Home.js
+++ blog-nutri/src/pages/Home/Home.js
@@ -2,38 +2,43 @@ import React, { useState } from 'react'
 import styles from './home.module.css'
 
 import { useFethingDocuments } from '../../hoock/useFecthingdates'
+import { Link, useNavigate } from 'react-router-dom'
+import DetalsPost from '../../Component/Post.details'
 
 
 const Home = () => {
-    const [send, setSend]  = useState('')
-     
-    const{document:post, loading, error} = useFethingDocuments("posts")
+    const [query, setQuery]  = useState('')
+    
+    const navigate = useNavigate()
+    const{document:post, loading} = useFethingDocuments("posts")
     console.log('this is null', post)
+    
     const HandSubmit = (e) => {
         e.preventDefault("")
+      
+       if(query) {
+         navigate(`/search/?q=${query}`)
 
+       }
+       
     }
 
     return( <div className={styles.home} >
+        
         <h3 >Blog sobre saúde.</h3>
-       
+        
         <form onSubmit={HandSubmit} className={styles.search_form} >
          <label>
-            <input type="text"   value={send}  placeholder='Buscar'  onChange={(e) => setSend(e.target.value)}  />
+            <input type="text"   placeholder='Buscar'  onChange={(e) => setQuery(e.target.value)}  />
          </label>
 
          <button className='btn btn-dark' >Search</button>
 
         </form >
+        {loading && <p>carregando posts ..... </p> }
     
-    
-         {post && post.map((posts) => ( 
-          <div className='d-flex justify-content-center align-items-center' >
-          <p key={posts.id}> testing here now  {posts.title} -
-            <img className='rounded'  src={posts.image} alt={posts.title} />
-        
-          </p> 
-          </div>))}
+         {post && post.map((posts) =><DetalsPost key={posts.id} posts={posts} /> 
+         )}
         
         {!post && <p className='text-center bg-primary' > Nenhum post aqui ainda ....</p>  }  
     
