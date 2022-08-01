a5753c6812ee6cae9876f10d6f8464e48461deba
Felipe <machadofelipe2016@outlook.com>
2022-08-01 17:57:20 -0300

    c51-create all structure about search

--- blog-nutri/src/pages/Search/search.js
+++ blog-nutri/src/pages/Search/search.js
@@ -1,19 +1,35 @@
 import { Link } from 'react-router-dom'
 
+import { useSearchParams } from 'react-router-dom'
 import React from 'react'
 import { useFethingDocuments } from '../../hoock/useFecthingdates'
 
+import DetalsPost from '../../Component/Post.details'
 
-const SearchDates = () => {
+const SearchDeates = () => {
+   
+    const query = useSearchParams()
+    const search = query.get('q')
 
-  
+    const {document:post} = useFethingDocuments('posts', search)
+   
 
 
     return(<div className='d-flex justify-content-center align-items-center' >
            
-           <h1>Testing searching here!!</h1>
+        <h1>Verify Search!</h1>
+          
+         {post && post.lenght === 0 && (<p>
+            Nenhum post encontrado aqui!!!
+            <Link className='btn bg-warning align-items-center' > Voltar </Link>
+         </p>) } 
+
+
+        {post && post.map((posts) => (
+         <DetalsPost  key={posts.id}  posts={posts} />
+        ))}
 
     </div>)
 }
 
-export default SearchDates
+export default SearchDeates
