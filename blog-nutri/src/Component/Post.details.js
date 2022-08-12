aaf605eccc5f1ff9a57a8eedbcd17a8a2a4f9371
Felipe <machadofelipe2016@outlook.com>
2022-08-12 13:38:20 -0300

    c84-adding another things

--- blog-nutri/src/Component/Post.details.js
+++ blog-nutri/src/Component/Post.details.js
@@ -1,20 +1,31 @@
 import { Link } from "react-router-dom"
+import styles from './post.module.css'
+
+import {BsXSquareFill} from 'react-icons/bs'
 
 const DetalsPost = ({posts}) => {
     console.log("testing here", posts)
-    return(<div className="align-items-center " >
-            <img src={posts.image}  alt={posts.title} className='img-fluid' />
-            <h2 className="mx-2" >{posts.title}</h2>
-            <p className="mx-2" >{posts.createdBy}</p>
-
-
-        {posts && posts.tags.map((tag) => (<p className="flex mx-2 " key={tag} >
-            <span> {tag} </span> 
-        </p>))}
+    //   <BsXSquareFill/> 
+    
+    return(<div className="text-center" >
         
-         
-         <Link   to={`/post/${posts.id}`} className="btn bg-danger" >Ler.</Link>
+        <div className=" container-sm  d-flex justify-content-center align-items-center column my-2 " >  
+        <div className={styles.sizecard} >
+        <p className="badge float-start"> Feed</p>
+        
+        <h2 className="text-break" >{posts.title}</h2>
+        <h5 className="text-break fst-italic" > {posts.body} </h5>
+        <p >{posts.createdBy}</p>
+     
+        <Link to={`/post/meuspost`} >  <BsXSquareFill/> </Link>
 
+        <button className="btn"  >Excluir
+        </button>
+
+        
+        </div>
+        
+      </div> 
     </div>)
 
 }
