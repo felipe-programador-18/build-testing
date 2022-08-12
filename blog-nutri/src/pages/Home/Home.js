5f1b6b0e39f6b33aa79467fd66955392c33719cc
Felipe <machadofelipe2016@outlook.com>
2022-08-12 13:28:32 -0300

    c77-created new Structure about my home

--- blog-nutri/src/pages/Home/Home.js
+++ blog-nutri/src/pages/Home/Home.js
@@ -1,51 +1,105 @@
 import React, { useState } from 'react'
 import styles from './home.module.css'
-
+import './bi.jpg'
 import { useFethingDocuments } from '../../hoock/useFecthingdates'
 import { Link, useNavigate } from 'react-router-dom'
 import DetalsPost from '../../Component/Post.details'
+import { useValueAuth } from "../../Contextmanage/authcontext"
+import { UseInsertDocument } from "../../hoock/useInsertDocument"
+
+
 
 
 const Home = () => {
-    const [query, setQuery]  = useState('')
-    
+    const[title, setTitle] = useState('')
+    const [body, setBody] = useState('') 
+    const [errorform,setErrorFor] = useState('')
+    const{user} = useValueAuth()
+
+
     const navigate = useNavigate()
-    const{document:post, loading} = useFethingDocuments("posts")
+    const{document:post, loading} =  useFethingDocuments("posts")
+    
+
+
+    const {InsertDocument,response} = UseInsertDocument("posts")
     
     const HandSubmit = (e) => {
-        e.preventDefault("")
+      e.preventDefault("")
+      setErrorFor("")
       
-       if(query) {
-         navigate(`/search/?q=${query}`)
-
+      if(!title || !body ){
+        setErrorFor("Por favor preencha todos os campos!")
        }
+     
+    if(errorform) return;
+ 
+      InsertDocument({
+       title,
+       body,
+       uid: user.uid,
+       createdBy: user.displayName
+      })
+ 
+        
+      navigate("/post/meuspost")
        
     }
+ 
+    //className='d-flex d-column align-items-center justify-content-center'
 
-    return( <div className={styles.home} >
+    return( <div className='d-flex d-column align-items-center justify-content-center container-xl ' >
         
-        <h3 >Blog sobre saúde.</h3>
+        <form onSubmit={HandSubmit} >  
+        <div className={styles.size}>
+         <h2>BuildBox</h2>
         
-        <form onSubmit={HandSubmit} className={styles.search_form} >
-         <label>
-            <input type="text"   placeholder='Buscar'  onChange={(e) => setQuery(e.target.value)}  />
-         </label>
+          <div className="mb-3 mx-2">
+           
 
-         <button className='btn btn-dark' >Search</button>
+          <input type="text" 
+          className="form-control bg-light opacity-25 text-bold "
+          name='title'
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Digite seu Nome."/>
+          
+          </div>
+          <div className="mb-b mx-2">
+        
+          <textarea 
+           className="form-control bg-light opacity-25"  id="exampleFormControlTextarea1"
+           name='body'
+           value={body}
+           onChange={(e) => setBody(e.target.value) }
+           rows="3" 
+           placeholder="Mensagem."></textarea>
+          
+          </div>
 
-        </form >
-        {loading && <p>carregando posts ..... </p> }
+          
+          {!response.loading &&
+          <button className='btn btn-dark my-4 mx-2 float-end' >Publicar</button> }
+          
+          {response.loading && (<button className='btn btn-dark my-4 mx-2 float-end' >Carregando...</button> )}
+        
+        
+         </div>  
+        
+         
+         {errorform && <p>{errorform}</p> }
+         {loading && <p>carregando posts ..... </p> }
     
+        
+         {post &&  post.length === 0 && ( <div>    <h1 className='text-center' > Nenhum post aqui ainda ....</h1>
+         <Link to={'/post/create'} className='btn lean' >Crie seu Primeiro post.</Link>
+          </div>) } 
+
          {post && post.map((posts) =><DetalsPost key={posts.id} posts={posts} /> 
          )}
-        
-        {post &&  post.length === 0 && ( <div>    <h1 className='text-center' > Nenhum post aqui ainda ....</h1>
-        <Link to={'/post/create'} className='btn lean' >Crie seu Primeiro post.</Link>
-        </div>) }  
-    
-    
-       
-    
+
+ 
+     </form> 
     
     </div>  )
 }
