7fbee6424a5c9dac6748664aebf22b6d81c6926f
Felipe <machadofelipe2016@outlook.com>
2022-08-13 17:23:01 -0300

    c100-adding dash component

--- blog-nutri/src/Component/Post.details.js
+++ blog-nutri/src/Component/Post.details.js
@@ -7,7 +7,7 @@ const DetalsPost = () => {
   <div className="text-center" > 
     
     {<div className=" container-sm  d-flex justify-content-center align-items-center column my-2 " >  
-     <Dashboard/> 
+      <Dashboard/>         
      </div> 
     }
     
